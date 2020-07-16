3abb21f24e7f18ef48d42c64c13552adaccd8f79
Nikolai Urman <unik16372@gmail.com>
2020-07-16 17:26:15 +0300

    Just started

---
+++ FinderForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Finder
+{
+    partial class FinderForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultTreeView = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.findButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.filesCountLabel = new System.Windows.Forms.Label();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resultTreeView
+            // 
+            this.resultTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTreeView.Location = new System.Drawing.Point(15, 56);
+            this.resultTreeView.Name = "resultTreeView";
+            this.resultTreeView.Size = new System.Drawing.Size(484, 297);
+            this.resultTreeView.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(82, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(336, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя файла:";
+            // 
+            // findButton
+            // 
+            this.findButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.findButton.Location = new System.Drawing.Point(424, 12);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(75, 23);
+            this.findButton.TabIndex = 3;
+            this.findButton.Text = "Найти";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат поиска:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Обрабатываемый файл:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 369);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Количество обработанных файлов:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Прошедшее время:";
+            // 
+            // filesCountLabel
+            // 
+            this.filesCountLabel.AutoSize = true;
+            this.filesCountLabel.Location = new System.Drawing.Point(203, 369);
+            this.filesCountLabel.Name = "filesCountLabel";
+            this.filesCountLabel.Size = new System.Drawing.Size(0, 13);
+            this.filesCountLabel.TabIndex = 8;
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(148, 356);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.fileNameLabel.TabIndex = 9;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(357, 369);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 13);
+            this.timeLabel.TabIndex = 10;
+            // 
+            // FinderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 391);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.filesCountLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resultTreeView);
+            this.Name = "FinderForm";
+            this.Text = "Поиск файлов";
+            this.Load += new System.EventHandler(this.FinderForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView resultTreeView;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label filesCountLabel;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ FinderForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Finder
+{
+
+    public partial class FinderForm : Form
+    {
+
+        private static TreeNode mainNode;
+
+        public FinderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            PrepareToStart(@"G:\");
+            Finder f = new Finder();
+            f.FindFiles(@"G:\", "444.pas", this);
+        }
+
+        private void PrepareToStart(String startDirectory)
+        {
+            mainNode = new TreeNode(startDirectory);
+            mainNode.ImageKey = "folder";
+            resultTreeView.Nodes.Clear();
+            resultTreeView.Nodes.Add(mainNode);
+        }
+
+        public void AddNode(String text, byte type)
+        {
+            TreeNode aNode;
+            aNode = new TreeNode(text, 0, 0);
+            aNode.ImageKey = "file";
+            mainNode.Nodes.Add(aNode);
+            resultTreeView.ExpandAll();
+        }
+
+        public void AddCheckingFileInfo(String text)
+        {
+            fileNameLabel.Text = text;
+        }
+
+        private void FinderForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    class Finder
+    {
+        public void FindFiles (String startDirectory, String fileName, FinderForm mainForm)
+        {
+            ScanDir(startDirectory, fileName, mainForm);
+        }
+
+        public void ScanDir(String dirName, String filename, FinderForm mainForm)
+        {
+            DirectoryInfo dir = new DirectoryInfo(dirName);
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                mainForm.AddCheckingFileInfo(file.Name);
+                if (filename == file.Name)
+                    mainForm.AddNode(file.Name, 0);
+            }
+        }
+    }
+}
